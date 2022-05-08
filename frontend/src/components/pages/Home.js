8a8e97f7aa410297bbedf9d035582ecd87ab3a41
Andrelise <andrelisebuttner@hotmail.com>
2022-05-08 18:11:13 -0300

    Criando um web page com react.js e docker.

---
+++ frontend/src/components/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../../App.css';
+import Cards from '../Cards';
+import HeroSection from '../HeroSection';
+import Footer from '../Footer';
+
+function Home() {
+  return (
+    <React.Fragment>
+      <HeroSection />
+      <Cards />
+      <Footer />
+    </React.Fragment>
+
+  );
+}
+
+export default Home;
